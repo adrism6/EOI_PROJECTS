c0664a72b4a4305fd48f3ad713d94bfc39c03339
adrism6 <adrian6101995@gmail.com>
2020-07-05 00:37:55 +0100

    Adding New_file, Rename and Delete functions + fix few errors

--- Project4_DesktopApps/pynotepad.py
+++ Project4_DesktopApps/pynotepad.py
@@ -1,13 +1,12 @@
 from PyQt5.QtGui import QKeySequence, QTextDocument, QFont, QCursor, QMouseEvent
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt, QDir, QEvent
-from os.path import expanduser
-from os import getcwd
-import os
+from os.path import expanduser, isdir
+from os import getcwd, remove
 
 
 app = QApplication([])
-app.setApplicationName("PyNotepad")
+app.setApplicationName("PokéPad")
 editor = QPlainTextEdit()
 editor.document().setDefaultFont(QFont("monospace"))
 
@@ -35,7 +34,7 @@ class MyMainWindow(QMainWindow):
 
 
 window = MyMainWindow()
-window.setWindowTitle("PyNotepad")
+window.setWindowTitle("PokéPad")
 window.setCentralWidget(editor)
 
 file_menu = window.menuBar().addMenu("&File")
@@ -118,17 +117,47 @@ file_menu.addAction(close_action)
 def show_about_dialog():
     text = """
         <center>
-            <h1>PyNotepad</h1><br/>
-            <img src=logo.png width=200 height=200>
+            <h1>PokéPad</h1><br/>
+            <img src=pokepad.png width=300 height=450>
+            <p>Version 0.0.2</p>
+            <p>Date: 05/07/2020</p>
+            <p>Last updates at:</p>
+            <a href="https://github.com/adrism6/EOI_PROJECTS/tree/master/Project4_DesktopApps">Github</a>
         </center>
-        <p>Version 0.0.1</p>
     """
-    QMessageBox.about(window, "About PyNotepad", text)
+    QMessageBox.about(window, "About PokéPad", text)
+
+
+def show_tools_dialog():
+    text = """
+        <b><p>Main Menu:</p></b>
+            <p>- New document: Opens an empty document without an associated directory.</p>
+            <p>- Open file: Opens an existing file.</p>
+            <p>- Save: Saves the changes of the file. If the file is new, name it and select a directory.</p>
+            <p>- Close: Closes the application.</p>
+        <b><p>TreeView doble click:</p></b>
+            <p>- On directories: Opens and closes the directory on the treeview.</p>
+            <p>- On files: Opens on the editor, if file is acceptable.</p>
+        <b><p>TreeView right click:</p></b>
+            <p>- Open: Opens files on the editor, if file is acceptable.</p>
+            <p>- New file: Opens an empty file with the given name and directory.</p>
+            <p>- Rename: Renames the selected file and places it in the selected directory.</p>
+            <p>- Delete: Deletes the selected file.</p>
+    """
+    QMessageBox.about(window, "Poképad tools", text)
 
 
 help_menu = window.menuBar().addMenu("&Help")
-about_action = QAction("&About")
+
+tools_action = QAction("&Tools")
+tools_action.triggered.connect(show_tools_dialog)
+tools_action.setShortcut(QKeySequence("Ctrl+T"))
+
+about_action = QAction("A&bout")
 about_action.triggered.connect(show_about_dialog)
+about_action.setShortcut(QKeySequence("Ctrl+B"))
+
+help_menu.addAction(tools_action)
 help_menu.addAction(about_action)
 
 
@@ -144,6 +173,7 @@ class MyTreeView(QTreeView):
         # estaria bien implementar que se abra desde home hasta el directorio actual
         self.tree.doubleClicked.connect(self.open_file)
         self.tree.setSortingEnabled(True)
+        self.tree.setColumnWidth(0, 150)  # estaria bien implementar que se ajuste solo
         self.tree.setContextMenuPolicy(Qt.CustomContextMenu)
         self.tree.customContextMenuRequested.connect(self.context_menu)
         dockwidget = QDockWidget("TreeView")
@@ -151,17 +181,17 @@ class MyTreeView(QTreeView):
         dockwidget.setAllowedAreas(Qt.LeftDockWidgetArea)
         global window
         window.addDockWidget(Qt.LeftDockWidgetArea, dockwidget)
-        # treeview.setColumnWidth(0, 150)  # estaria bien implementar que se ajuste solo
 
     def context_menu(self):
-        global open_action
         menu = QMenu()
         open = menu.addAction("Open")
         open.triggered.connect(self.open_file)
+        new_file = menu.addAction("New file")
+        new_file.triggered.connect(self.new_file)
+        rename = menu.addAction("Rename")
+        rename.triggered.connect(self.rename_file)
         delete = menu.addAction("Delete")
         delete.triggered.connect(self.delete_file)
-        rename = manu.addAction("Rename")
-        rename.triggered.connect(self.rename_file)
         cursor = QCursor()
         menu.exec_(cursor.pos())
 
@@ -170,18 +200,54 @@ class MyTreeView(QTreeView):
         if check_is_modified() == True:
             index = self.tree.currentIndex()
             filename = self.model.filePath(index)
-            if filename:
+            isDirectory = isdir(filename)
+            if isDirectory == True:
+                return
+            else:
                 file_contents = ""
                 with open(filename, "r") as f:
                     file_contents = f.read()
                 editor.setPlainText(file_contents)
-            file_path = filename
+                file_path = filename
 
-    def delete_file(self):
-        pass
+    def new_file(self):
+        global file_path
+        if check_is_modified() == True:
+            newname, _ = QFileDialog.getSaveFileName(window, "Open as...")
+            if newname:
+                editor.clear()
+                file_contents = ""
+                with open(newname, "w") as f:
+                    f.write(file_contents)
+                editor.setPlainText(file_contents)
+                file_path = newname
 
     def rename_file(self):
-        pass
+        global file_path
+        if check_is_modified() == True:
+            index = self.tree.currentIndex()
+            filename = self.model.filePath(index)
+            newname, _ = QFileDialog.getSaveFileName(window, "Rename as...")
+            if newname:
+                with open(filename, "r") as f:
+                    with open(newname, "w") as fw:
+                        for line in f:
+                            fw.write(line)
+                editor.document().setModified(False)
+                if filename == file_path:
+                    file_path = newname
+                remove(filename)
+                return True
+            return False
+
+    def delete_file(self):
+        global file_path
+        index = self.tree.currentIndex()
+        filename = self.model.filePath(index)
+        if file_path == filename:
+            editor.clear()
+            file_path = None
+        remove(filename)
 
 
 treeview = MyTreeView()
